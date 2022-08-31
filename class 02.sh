db2df59c32f9ba16c2c97dd7c2fa36b280d02e57
Su22 <suzan.thapa2221@gmail.com>
2022-08-30 18:10:10 -0700

    Create class 02.sh

---
+++ class 02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: Ops 301 
+# Author: sujan thapa magar 
+# Date of latest revision: 8/30/2022
+# Purpose: Copies log to working directory and appends the current date and time to the filename
+
+# Main
+
+day=$(date +%D%T)
+
+cp /var/log/syslog .
+
+mv syslog syslog_$day
+
+# End
