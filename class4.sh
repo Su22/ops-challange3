e54dbea38d8b802718d56d43baacbcef657b1d92
Su22 <suzan.thapa2221@gmail.com>
2022-09-01 19:09:20 -0700

    Create class4.sh

---
+++ class4.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script: Ops 301 Class 04 Ops Challenge Solution
+# Author: sujan thapa magar              
+# Date of latest revision: 01SEP2022      
+# Purpose: Menu system with options that do different things and does not end until the user selects exit
+
+# Main
+
+PS3="select one of the following menu options: "
+
+select number in Print_Hello_World Ping_Loopback Print_IP_Information Exit 
+do 
+    case $number in
+        Print_Hello_World) 
+        echo "----------------------------"
+        echo "Hello World"
+        echo "----------------------------"
+        ;;
+        Ping_Loopback)
+        echo "----------------------------"
+        echo "loopback ping: "
+        ping -c 5 127.0.0.1
+        echo "----------------------------"
+        ;;
+        Print_IP_Information)
+        echo "----------------------------"
+        echo "computer's IP informaion: "
+        echo "----------------------------"
+        ifconfig
+        ;;
+        Exit)
+        echo "----------------------------"
+        echo "done!"
+        exit
+
+    esac
+done
+
+# End
