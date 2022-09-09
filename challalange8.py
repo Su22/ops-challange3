5b2434f39b07e39b39c9b1b8bdda60521cb9bc91
Su22 <suzan.thapa2221@gmail.com>
2022-09-08 17:40:17 -0700

    Create challalange8.py

---
+++ challalange8.py
@@ -0,0 +1,10 @@
+# Script: Ops 301 Class 12 Ops Challenge Solution
+# Author: sujan thapa magar
+# Date of latest revision: September 08, 2022
+# Purpose: This powershell script adds a user to AD
+
+# Main
+
+New-ADUser -Name "Franz Ferdinand" -Title "TPS Reporting Lead" -Company "Globex USA" -Office "Springfield, OR" -Department "TPS" -Email Address "ferdi@GlobeXpower.com"
+
+# End 
