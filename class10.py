4527889b863584de87f2ab6fe526929a2a72f25a
Su22 <suzan.thapa2221@gmail.com>
2022-09-12 19:54:20 -0700

    Create class10.py

---
+++ class10.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+# Script: Ops 301d5 Ops Challenge: Class 09
+# Author: sujan thapa magar
+# Date of latest revision: 12 Sept 2022
+# Purpose: Create if statements using these logical conditionals below.
+
+
+# Assigned Variables
+a = 12
+b = 21
+
+# Main
+if a == b:
+    print ("a and b are equal")
+
+if a != b:
+    print ("a does not equal b")
+
+if a < b:
+    print ("b is less than a")
+
+if a <= b:
+    print ("a is less than or equal to b")
+
+if a > b:
+    print ("a is greater than b")
+
+if a >= b:
+    print ("a is greater than or equal to b")
+
+if a == b:
+    print ("a and b are equal")
+elif a != b:
+    print ("a and b are not equal")
+
+
+if a > b:
+    print ("a is greater than b")
+elif a < b:
+    print ("a is less than b")
+else:
+    print ("a and b are equal")
+
+# End
